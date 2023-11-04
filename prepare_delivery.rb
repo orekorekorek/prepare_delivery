30f93de4fc230312510ea6ab5c921f9f60cbb1d9
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2023-11-04 17:17:12 +0300

    feat(2) refactored

--- prepare_delivery.rb
+++ prepare_delivery.rb
@@ -1,25 +1,38 @@
-class PrepareDelivery
-  TRUCKS = { kamaz: 3000, gazel: 1000 }
+require 'date'
+require 'ostruct'
 
-  def initialize(order, user)
+class PrepareDelivery
+  TRUCKS = { kamaz: 3000, gazel: 1000 }.freeze
+
+  def initialize(order)
     @order = order
-    @user = user
   end
 
   def perform(destination_address, delivery_date)
-    result = { truck: nil, weight: nil, order_number: @order.id, address: destination_address, status: :ok }
-    raise 'Дата доставки уже прошла' if delivery_date < Time.current
-    if destination_address.city.empty? || destination_address.street.empty? || destination_address.house.empty?
-      raise 'Нет адреса'
-    end
+    result = { truck: nil, weight: nil, order_number: order.id, address: destination_address, status: :ok }
 
-    weight = @order.products.map(&:weight).sum
-    TRUCKS.keys.each { |key| result[:truck] = key if TRUCKS[key.to_sym] > weight }
-    raise 'Нет машины' if result[:truck].nil?
+    validate_delivery_date!(delivery_date)
+    destination_address.validate!
 
-    result
-  rescue StandardError
-    result[:satus] = 'error'
+    weight = order.total_weight
+    result[:truck] = pick_truck(weight)
+  rescue ValidationError
+    result[:status] = 'error'
+  end
+
+  private
+
+  attr_reader :order
+
+  def validate_delivery_date!(delivery_date)
+    raise ValidationError, 'Дата доставки уже прошла' if delivery_date < DateTime.now
+  end
+
+  def pick_truck(weight)
+    truck = TRUCKS.keys.each { |key| key if TRUCKS[key.to_sym] > weight }
+    return truck if truck
+
+    raise ValidationError, 'Нет машины'
   end
 end
 
@@ -30,6 +43,10 @@ class Order
 
   def products
     [OpenStruct.new(weight: 20), OpenStruct.new(weight: 40)]
+  end
+
+  def total_weight
+    products.map(&:weight).sum
   end
 end
 
@@ -45,6 +62,14 @@ class Address
   def house
     'д. 5'
   end
+
+  def validate!
+    return unless city.empty? || street.empty? || house.empty?
+
+    raise ValidationError, 'Нет адреса'
+  end
 end
 
-PrepareDelivery.new(Order.new, OpenStruct.new).perform(Address.new, Date.tomorrow)
+class ValidationError < StandardError; end
+
+PrepareDelivery.new(Order.new).perform(Address.new, DateTime.now + 1)
